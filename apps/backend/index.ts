df0495b3d6950c732624c1a6a1b55d2d8537f645
thebinodbist <thebinodbist@gmail.com>
2026-08-09 14:51:14 +0545

    ws refactor

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import  express  from 'express';
+import { prisma } from "db/client";
+
+
+const app = express();
+app.post("signup",async(req,res)=>{
+  prisma.user.create({
+    
+  })
+})
